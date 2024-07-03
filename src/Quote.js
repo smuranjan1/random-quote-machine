b234971424ea90110d0a57e3c964fbfd081b2f22
smuranjan1 <smyan.muranjan@gmail.com>
2024-07-03 18:08:57 -0500

    working on style

--- src/Quote.js
+++ src/Quote.js
@@ -1,10 +1,19 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons'
+import { faQuoteRight } from '@fortawesome/free-solid-svg-icons'
 
 export function Quote (props) {
     return(
         <>
-            <p id="text"> <FontAwesomeIcon icon={faQuoteLeft} /> {props.quote}</p>
+            <p id="text"> 
+                <span id="icon-wrapper">
+                    <FontAwesomeIcon icon={faQuoteLeft} />
+                </span>
+                {props.quote}
+                <span id="icon-wrapper">
+                    <FontAwesomeIcon icon={faQuoteRight} />
+                </span>
+            </p>
             <p id="author">— {props.author}</p>
         </>
     );
--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { TweetButton } from './TweetButton';
 import { Quote } from './Quote';
+import { colorPalettes } from './constants';
 import './QuoteBox.css';
 
 export function QuoteBox(props) {
@@ -8,7 +9,11 @@ export function QuoteBox(props) {
     const [quote, setQuote] = useState("Be the change that you wish to see in the world.");
     const [author, setAuthor] = useState("Mahatma Gandhi");
 
-    const getQuote = async () => {
+    const getQuoteAndChangeColors = async () => {
+        const rand = Math.floor(Math.random() * colorPalettes.length)
+        var color = colorPalettes[rand];
+        document.getElementById("quote-box").style.backgroundColor = color.colors[0];
+        document.querySelector("body").style.backgroundColor = color.colors[1];
         const response = await fetch("https://api.quotable.io/random");
         const data = await response.json();
         if (response.ok) {
@@ -24,7 +29,7 @@ export function QuoteBox(props) {
             <Quote quote={quote} author={author} />
             <div id="button-container">
                 <TweetButton quote={quote} author={author} />
-                <button id="new-quote" onClick={getQuote}>New Quote</button>
+                <button id="new-quote" onClick={getQuoteAndChangeColors}>New Quote</button>
             </div>
         </div>
     );
