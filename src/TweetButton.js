82cd85fd38cf80a1324ae26665b23d3c334b7e9a
smuranjan1 <smyan.muranjan@gmail.com>
2024-07-03 19:25:23 -0500

    Resolved warnings.

--- src/TweetButton.js
+++ src/TweetButton.js
@@ -2,6 +2,7 @@ export function TweetButton(props) {
    return(
     <a id="tweet-quote" className="primary-button"
     href={`https://twitter.com/intent/tweet?text="${props.quote}" ${props.author}`} 
+    rel="noreferrer"
     target="_blank">
       <div id="tweet-text">Tweet</div>
    </a>
